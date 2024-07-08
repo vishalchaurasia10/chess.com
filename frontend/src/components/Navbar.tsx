7301d3b798b9a2b9f4e3834d0b268120e70948a2
vishalchaurasia10 <1ms21cs138@msrit.edu>
2024-07-09 02:06:28 +0530

    fixed the navbar for pc

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,48 +1,38 @@
-import { navData } from '@/utils/constants';
 import Link from 'next/link';
-import React from 'react';
-import { BsHouse, BsPeopleFill } from 'react-icons/bs';
-import { IoGameController } from 'react-icons/io5';
+import { BsPlus, BsGearFill, BsPeopleFill } from 'react-icons/bs';
 import { SiChessdotcom } from 'react-icons/si';
 
-const NavIcon = ({ type }: { type: String }) => {
+const SideBar = () => {
     return (
-        <div className='flex items-center space-x-3 font-firaCode bg-[rgba(255,255,255,0.2)] py-1 px-3 mx-3 rounded-lg'>
-            {
-                type === 'Home' ? <BsHouse /> :
-                    type === 'Play' ? <IoGameController /> :
-                        type === 'Watch' ? <BsPeopleFill /> : ''
-            }
-            <span className='font-bold text-xl'>{type}</span>
+        <div className="fixed z-50 top-0 left-0 h-screen w-16 flex flex-col bg-[rgba(255,255,255,0.2)] shadow-lg">
+            <SideBarIcon icon={<SiChessdotcom size="28" />} text='Chess.com' toUrl='/' />
+            <Divider />
+            <SideBarIcon icon={<BsPlus size="32" />} text='Play new game ♟️' toUrl='/game' />
+            <SideBarIcon icon={<BsPeopleFill size="20" />} text='Watch other games 👀' toUrl='/watch' />
+            <Divider />
+            <SideBarIcon icon={<BsGearFill size="22" />} text='Sign In ⚙️' toUrl='/sign-in' />
         </div>
     );
 };
 
-const Navbar = () => {
-    return (
-        <nav className='fixed z-50 top-0 left-0 h-full p-2 px-0 bg-black flex flex-col items-center w-min font-firaCode'>
-            <div className="top h-[10%] py-2 flex items-center justify-start space-x-1">
-                <SiChessdotcom className='text-5xl text-[#acd06f]' />
-                <span className='font-bold text-2xl'>Chess</span>
-            </div>
-            <div className="center h-[70%] pt-8">
-                <ul className='flex flex-col space-y-3'>
-                    {navData.map((item, index) => (
-                        <li key={index}>
-                            <Link className='text-3xl' href={item.href}>
-                                <NavIcon type={item.name} />
-                            </Link>
-                        </li>
-                    ))}
-                </ul>
-            </div>
-            <div className="bottom h-[20%] flex items-end pb-8">
-                <button className='p-2 px-4 text-black font-bold rounded-lg bg-[#acd06f] hover:-translate-y-1 transition-all duration-300'>
-                    Sign In
-                </button>
-            </div>
-        </nav>
-    );
+type SideBarIconProps = {
+    icon: React.ReactNode;
+    text?: string;
+    toUrl: string;
 };
 
-export default Navbar;
+const SideBarIcon = ({ icon, text = 'tooltip 💡', toUrl = '/' }: SideBarIconProps) => (
+    <Link href={toUrl}>
+        <div className="sidebar-icon group">
+            {icon}
+            <span className="sidebar-tooltip group-hover:scale-100">
+                {text}
+            </span>
+        </div>
+    </Link>
+);
+
+
+const Divider = () => <hr className="sidebar-hr" />;
+
+export default SideBar;
