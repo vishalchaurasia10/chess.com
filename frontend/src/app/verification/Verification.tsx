cf76f3fb8f5cb175c0ddafe25c6e0169e793cc11
vishalchaurasia10 <1ms21cs138@msrit.edu>
2024-07-12 05:41:40 +0530

    fixed suspense error

--- frontend/src/app/verification/Verification.tsx
+++ frontend/src/app/verification/Verification.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React, { useContext, useEffect, useState } from 'react'
+import React, { useContext, useEffect, useState, Suspense } from 'react'
 import { AiFillCheckCircle, AiFillExclamationCircle } from 'react-icons/ai';
 import { useSearchParams, useRouter } from 'next/navigation';
 import { AuthContext } from '@/context/Auth/authContext';
@@ -7,7 +7,8 @@ import { AuthContext } from '@/context/Auth/authContext';
 const Verification = () => {
 
     const [verified, setVerified] = useState(false)
-    const token = useSearchParams().get('token')
+    const searchParams = useSearchParams();
+    const token = searchParams.get('token')
     const authContext = useContext(AuthContext)
     if (!authContext) {
         throw new Error('AuthContext is not defined')
@@ -21,28 +22,29 @@ const Verification = () => {
         } else {
             router.push('/sign-in')
         }
-    }, [])
+    }, [token, router])
 
     useEffect(() => {
         if (user) {
             setVerified(true)
             router.push('/game')
         }
-    }, [user])
+    }, [user, router])
 
     return (
-        <>
-            {(verified ?
+        <Suspense fallback={<div>Loading...</div>}>
+            {verified ? (
                 <div className={`h-screen font-roboto font-poppins text-center flex flex-col items-center justify-center space-y-4 -mt-24`}>
                     <AiFillCheckCircle className='text-green-500 text-9xl' />
                     <h1 className='text-black text-4xl md:text-5xl font-bold md:pb-4 lg:pb-0 xl:pb-4 lg:py-2'>Email verified successfully</h1>
                 </div>
-                :
+            ) : (
                 <div className={`h-screen font-roboto text-center flex flex-col items-center justify-center space-y-4 -mt-24`}>
                     <AiFillExclamationCircle className='text-orange-400 text-9xl' />
                     <h1 className='text-black text-4xl md:text-5xl font-bold md:pb-4 lg:pb-0 xl:pb-4 lg:py-2'>Email verifying...</h1>
-                </div>)}
-        </>
+                </div>
+            )}
+        </Suspense>
     )
 }
 
